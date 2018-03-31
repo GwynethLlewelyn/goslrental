a0de34e0b4cd3154357beeb629bc8057f50470a1
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2018-03-31 19:57:32 +0100

    Re-using code from another app to get this done. Still doesn’t compile, needs more checking!

---
+++ backoffice.go
@@ -0,0 +1,264 @@
+// Functions to deal with the backoffice.
+package main
+
+import (
+//	"bytes"
+	"crypto/md5"
+	"database/sql"
+	"encoding/hex"
+	"fmt"
+	"github.com/gorilla/securecookie"
+	"github.com/heatxsink/go-gravatar"
+	"html/template"
+//	"io/ioutil"
+	"net/http"
+//	"strconv"
+//	"strings"
+)
+
+// GoSLRentalTemplatesType expands on template.Template.
+//  need to expand it so I can add a few more methods here 
+type GoSLRentalTemplatesType struct{
+	template.Template
+}
+
+// GoSLRentalTemplates stores all parsed templates for the backoffice.
+var GoSLRentalTemplates GoSLRentalTemplatesType
+
+// init parses all templates and puts it inside a (global) var.
+//  This is supposed to be called just once! (in func main())
+func (gt *GoSLRentalTemplatesType)init(globbedPath string) error {
+	temp, err := template.ParseGlob(globbedPath)
+	checkErr(err) // move out later, we just need it here to check what's wrong with the templates (20170706)
+	gt.Template = *temp;
+	return err
+}
+
+// GoSLRentalRenderer assembles the correct templates together and executes them.
+//  this is mostly to deal with code duplication 
+func (gt *GoSLRentalTemplatesType)GoSLRentalRenderer(w http.ResponseWriter, r *http.Request, tplName string, tplParams templateParameters) error {
+	thisUserName :=  getUserName(r)
+	
+	// add cookie to all templates
+	tplParams["SetCookie"] = thisUserName
+
+	// add Gravatar to templates (note that all logins are supposed to be emails)
+	
+	// calculate hash for the Gravatar hovercard
+	hasher := md5.Sum([]byte(thisUserName))
+	hash := hex.EncodeToString(hasher[:])
+	tplParams["GravatarHash"] = hash // we ought to cache this somewhere
+	
+	// deal with sizes, we want to have a specific size for the top menu
+	var gravatarSize, gravatarSizeMenu = 32, 32
+
+	// if someone set the sizes, then use them; if not, use defaults
+	// note that this required type assertion since tplParams is interface{}
+	// see https://stackoverflow.com/questions/14289256/cannot-convert-data-type-interface-to-type-string-need-type-assertion
+	if tplParams["GravatarSize"] == nil {
+		tplParams["GravatarSize"] = gravatarSize
+	} else {
+		gravatarSize = tplParams["GravatarSize"].(int)
+	}
+	if tplParams["GravatarSizeMenu"] == nil {
+		tplParams["GravatarSizeMenu"] = gravatarSizeMenu
+	} else {
+		gravatarSizeMenu = tplParams["GravatarSizeMenu"].(int)
+	}
+	// for Retina displays; we could add a multiplication function for Go templates, but I'm lazy (20170706)
+	tplParams["GravatarTwiceSize"] = 2 * gravatarSize
+	tplParams["GravatarTwiceSizeMenu"] = 2 * gravatarSizeMenu
+	
+	// Now call the nice library function to get us the URL to the image, for the two sizes
+	g := gravatar.New("identicon", gravatarSize, "g", true)
+	tplParams["Gravatar"] = g.GetImageUrl(thisUserName) // we also ought to cache this somewhere
+	
+	g = gravatar.New("identicon", gravatarSizeMenu, "g", true)
+	tplParams["GravatarMenu"] = g.GetImageUrl(thisUserName) // we also ought to cache this somewhere
+	
+	w.Header().Set("X-Clacks-Overhead", "GNU Terry Pratchett") // do a tribute to one of my best fantasy authors (see http://www.gnuterrypratchett.com/) (20170807)
+	return gt.ExecuteTemplate(w, tplName, tplParams)
+}
+
+// Auxiliary functions for session handling
+//  see https://mschoebel.info/2014/03/09/snippet-golang-webapp-login-logout/ (20170603)
+
+var cookieHandler = securecookie.New(		// from gorilla/securecookie
+    securecookie.GenerateRandomKey(64),
+    securecookie.GenerateRandomKey(32))
+
+// setSession returns a new session cookie with an encoded username.
+func setSession(userName string, response http.ResponseWriter) {
+	value := map[string]string{
+		"name": userName,
+	}
+	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+		cookie := &http.Cookie{
+			Name:	"session",
+			Value: encoded,
+			Path:	"/",
+		}
+		// Log.Debug("Encoded cookie:", cookie)
+		http.SetCookie(response, cookie)
+	} else {
+		Log.Error("Error encoding cookie:", err)
+	}
+ }
+ 
+ // getUserName sees if we have a session cookie with an encoded user name, returning nil if not found.
+func getUserName(request *http.Request) (userName string) {
+	if cookie, err := request.Cookie("session"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+			userName = cookieValue["name"]
+		}
+	}
+	return userName
+}
+
+// clearSession will remove a cookie by setting its MaxAge to -1 and clearing its value.
+func clearSession(response http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:	"session",
+		Value:	 "",
+		Path:	 "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(response, cookie)
+}
+
+// checkSession will see if we have a valid cookie; if not, redirects to login.
+func checkSession(w http.ResponseWriter, r *http.Request) {
+	// valid cookie and no errors?
+	if getUserName(r) == "" {
+		http.Redirect(w, r, URLPathPrefix + "/admin/login/", 302)	
+	}
+}
+
+
+// Function handlers for HTTP requests (main functions for this file)
+
+// backofficeMain is the main page, has some minor statistics, may do this fancier later on.
+func backofficeMain(w http.ResponseWriter, r *http.Request) {
+	checkSession(w, r) // make sure we've got a valid cookie, or else send to login page
+	// let's load the main template for now, just to make sure this works
+	
+	tplParams := templateParameters{ "Title": "GoSLRental Administrator Panel - main",
+	}
+	err := GoSLRentalTemplates.GoSLRentalRenderer(w, r, "main", tplParams)
+	checkErr(err)
+	return
+}
+
+// backofficeUserManagement deals with adding/removing application users. Just login(email) and password right now, no profiles, no email confirmations, etc. etc. etc.
+//  This is basically a stub for more complex user management, to be reused by other developments...
+//  I will not develop this further, except perhaps to link usernames to in-world avatars (may be useful)
+func backofficeUserManagement(w http.ResponseWriter, r *http.Request) {
+	checkSession(w, r)
+	tplParams := templateParameters{ "Title": "GoSLRental Administrator Panel - User Management",
+			"Content": "Hi there, this is the User Management template",
+			"URLPathPrefix": URLPathPrefix,
+			"GoSLRentalJS": "user-management.js",
+	}
+	err := GoSLRentalTemplates.GoSLRentalRenderer(w, r, "user-management", tplParams)
+	checkErr(err)
+	return
+}
+
+
+// backofficeLogin deals with authentication.
+func backofficeLogin(w http.ResponseWriter, r *http.Request) {
+	// Log.Debug("Entered backoffice login for URL:", r.URL, "using method:", r.Method)
+	if r.Method == "GET" {
+		tplParams := templateParameters{ "Title": "GoSLRental Administrator Panel - login",
+				"URLPathPrefix": URLPathPrefix,
+		}
+		err := GoSLRentalTemplates.GoSLRentalRenderer(w, r, "login", tplParams)
+		checkErr(err)
+	} else { // POST is assumed
+		r.ParseForm()
+        // logic part of logging in
+        email		:= r.Form.Get("email")
+        password	:= r.Form.Get("password")
+        
+        // Log.Debug("email:", email)
+        // Log.Debug("password:", password)
+        
+        if email == "" || password == "" { // should never happen, since the form checks this
+	        http.Redirect(w, r, URLPathPrefix + "/", 302)        
+        }
+        
+        // Check username on database
+        db, err := sql.Open(PDO_Prefix, GoSLRentalDSN)
+		checkErr(err)
+		
+		defer db.Close()
+	
+		// query
+		rows, err := db.Query("SELECT Email, Password FROM Users")
+		checkErr(err)
+
+		defer rows.Close()
+		
+		var (
+			Email string
+			Password string
+		)
+	 
+		// enhash the received password; I just use MD5 for now because there is no backoffice to create
+		//  new users, so it's easy to generate passwords manually using md5sum;
+		//  however, MD5 is not strong enough for 'real' applications, it's just what we also use to
+		//  communicate with the in-world scripts (20170604)
+		pwdmd5 := fmt.Sprintf("%x", md5.Sum([]byte(password))) //this has the hash we need to check
+	  
+		authorised := false // outside of the for loop because of scope
+	
+		for rows.Next() {	// we ought just to have one entry, but...
+			_ = rows.Scan(&Email, &Password)
+			// ignore errors for now, either it checks true or any error means no authentication possible
+			if Password == pwdmd5 {
+				authorised = true
+				break
+			}		
+		}
+		
+	    if authorised {
+	        // we need to set a cookie here
+	        setSession(email, w)
+	        // redirect to home
+	        http.Redirect(w, r, URLPathPrefix + "/admin", 302)
+		} else {
+			// possibly we ought to give an error and then redirect, but I don't know how to do that (20170604)
+			http.Redirect(w, r, URLPathPrefix + "/", 302) // will ask for login again
+		}
+		return
+	}
+}
+
+// backofficeLogout clears session and returns to login prompt.
+func backofficeLogout(w http.ResponseWriter, r *http.Request) {
+	clearSession(w)
+	http.Redirect(w, r, URLPathPrefix + "/", 302)
+}
+
+// backofficeLSLRegisterObject creates a LSL script for registering cubes, using the defaults set by the user.
+//  This is better than using 'template' LSL scripts which people may fill in wrongly, this way at least
+//   we won't get errors about wrong signature PIN or hostnames etc.
+func backofficeLSLRegisterObject(w http.ResponseWriter, r *http.Request) {
+	checkSession(w, r)
+	tplParams := templateParameters{ "Title": "GoSLRental LSL Generator - register object.lsl",
+			"URLPathPrefix": URLPathPrefix,
+			"Host": Host,
+			"ServerPort": ServerPort,
+			"LSLSignaturePIN": LSLSignaturePIN,
+			"LSL": "lsl-register-object", // this will change some formatting on the 'main' template (20170706)
+	}
+	// check if we have a frontend (it's configured on the config.toml file); if no, use the ServerPort
+	//  the 'frontend' will be nginx, Apache, etc. to cache replies from Go and serve static files from port 80 (20170706)
+	if FrontEnd == "" {
+		tplParams["ServerPort"] = ServerPort
+	}
+	err := GoSLRentalTemplates.GoSLRentalRenderer(w, r, "main", tplParams)
+	checkErr(err)
+	return
+}
--- goslrental.go
+++ goslrental.go
@@ -5,10 +5,29 @@ import (
 	"database/sql"
 	"log"
 	"github.com/cznic/ql"
+	"github.com/op/go-logging" // more complete package to log to different outputs; we start with file, syslog, and stderr; 
+	"github.com/spf13/viper" // to read config files
 	"runtime"
 	"path/filepath"
 	"time"
 )
+
+var (
+	// Default configurations, hopefully exported to other files and packages
+	// we probably should have a struct for this (or even several)
+	Host, GoSLRentalDSN, URLPathPrefix, PDO_Prefix, PathToStaticFiles,
+	ServerPort, FrontEnd, LSLSignaturePIN string
+	logFileName string = "goslrental.log"
+	logMaxSize, logMaxBackups, logMaxAge int // configuration for the go-logging logger
+	logSeverityStderr, logSeverityFile, logSeveritySyslog logging.Level // more configuration for the go-logging logger
+	Log = logging.MustGetLogger("goslrental")	// configuration for the go-logging logger, must be available everywhere
+	logFormat logging.Formatter	// must be initialised or all hell breaks loose
+)
+
+const NullUUID = "00000000-0000-0000-0000-000000000000" // always useful when we deal with SL/OpenSimulator...
+
+//type templateParameters map[string]string
+type templateParameters map[string]interface{}
 
 // setUp tries to create a table on the QL database for testing purposes.
 func setUp(db *sql.DB) error {
@@ -34,10 +53,181 @@ func setUp(db *sql.DB) error {
 	return nil
 }
 
+// loadConfiguration loads all the configuration from the config.toml file.
+// It's a separate function because we want to be able to do a killall -HUP goslrental to force the configuration to be read again.
+// Also, if the configuration file changes, this ought to read it back in again without the need of a HUP signal (20170811).
+func loadConfiguration() {
+	fmt.Print("Reading goslrental configuration:")	// note that we might not have go-logging active as yet, so we use fmt
+	// Open our config file and extract relevant data from there
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {
+		fmt.Println("Error reading config file:", err)
+		return	// we might still get away with this!
+	}
+	// Without these set, we cannot do anything
+	viper.SetDefault("goslrental.Host", "localhost") // to prevent bombing out with panics
+	Host = viper.GetString("goslrental.Host"); fmt.Print(".")
+	viper.SetDefault("goslrental.URLPathPrefix", "") // empty by default, but you might add a 'main' website for information later
+	URLPathPrefix = viper.GetString("goslrental.URLPathPrefix"); fmt.Print(".")
+	GoSLRentalDSN = viper.GetString("goslrental.GoSLRentalDSN"); fmt.Print(".")
+	viper.SetDefault("PDO_Prefix", "ql") // for now, nothing else will work anyway...
+	PDO_Prefix = viper.GetString("goslrental.PDO_Prefix"); fmt.Print(".")
+	viper.SetDefault("goslrental.PathToStaticFiles", "~/go/src/goslrental")
+	path, err := expandPath(viper.GetString("goslrental.PathToStaticFiles")); fmt.Print(".")
+	if err != nil {
+		fmt.Println("Error expanding path:", err)
+		path = ""	// we might get away with this as well
+	}
+	PathToStaticFiles = path
+	viper.SetDefault("goslrental.ServerPort", ":3333")
+	ServerPort = viper.GetString("goslrental.ServerPort"); fmt.Print(".")
+	FrontEnd = viper.GetString("goslrental.FrontEnd"); fmt.Print(".")
+	viper.SetDefault("goslrental.LSLSignaturePIN", "9876") // better than no signature at all
+	LSLSignaturePIN = viper.GetString("opensim.LSLSignaturePIN"); fmt.Print(".")
+	// logging options
+	viper.SetDefault("log.FileName", "log/goslrental.log")
+	logFileName = viper.GetString("log.FileName"); fmt.Print(".")
+	viper.SetDefault("log.Format", `%{color}%{time:2006/01/02 15:04:05.0} %{shortfile} - %{shortfunc} ▶ %{level:.4s}%{color:reset} %{message}`)
+	logFormat = logging.MustStringFormatter(viper.GetString("log.Format")); fmt.Print(".")
+	viper.SetDefault("log.MaxSize", 500)
+	logMaxSize = viper.GetInt("log.MaxSize"); fmt.Print(".")
+	viper.SetDefault("log.MaxBackups", 3)
+	logMaxBackups = viper.GetInt("log.MaxBackups"); fmt.Print(".")
+	viper.SetDefault("log.MaxAge", 28)
+	logMaxAge = viper.GetInt("log.MaxAge"); fmt.Print(".")
+	viper.SetDefault("log.SeverityStderr", logging.DEBUG)
+	switch viper.GetString("log.SeverityStderr") {
+		case "CRITICAL":
+			logSeverityStderr = logging.CRITICAL
+    	case "ERROR":
+			logSeverityStderr = logging.ERROR
+    	case "WARNING":
+			logSeverityStderr = logging.WARNING
+    	case "NOTICE":
+			logSeverityStderr = logging.NOTICE
+    	case "INFO":
+			logSeverityStderr = logging.INFO
+    	case "DEBUG":
+			logSeverityStderr = logging.DEBUG
+		// default case is handled directly by viper
+	}
+	fmt.Print(".")
+	viper.SetDefault("log.SeverityFile", logging.DEBUG)
+	switch viper.GetString("log.SeverityFile") {
+		case "CRITICAL":
+			logSeverityFile = logging.CRITICAL
+    	case "ERROR":
+			logSeverityFile = logging.ERROR
+    	case "WARNING":
+			logSeverityFile = logging.WARNING
+    	case "NOTICE":
+			logSeverityFile = logging.NOTICE
+    	case "INFO":
+			logSeverityFile = logging.INFO
+    	case "DEBUG":
+			logSeverityFile = logging.DEBUG
+	}
+	fmt.Print(".")
+	viper.SetDefault("log.SeveritySyslog", logging.CRITICAL) // we don't want to swamp syslog with debugging messages!!
+	switch viper.GetString("log.SeveritySyslog") {
+		case "CRITICAL":
+			logSeveritySyslog = logging.CRITICAL
+    	case "ERROR":
+			logSeveritySyslog = logging.ERROR
+    	case "WARNING":
+			logSeveritySyslog = logging.WARNING
+    	case "NOTICE":
+			logSeveritySyslog = logging.NOTICE
+    	case "INFO":
+			logSeveritySyslog = logging.INFO
+    	case "DEBUG":
+			logSeveritySyslog = logging.DEBUG
+	}
+	fmt.Print(".")
+	fmt.Println("read!")	// note that we might not have go-logging active as yet, so we use fmt
+	
+	// Setup the lumberjack rotating logger. This is because we need it for the go-logging logger when writing to files. (20170813)
+	rotatingLogger := &lumberjack.Logger{
+	    Filename:   logFileName,	// this is an option set on the config.yaml file, eventually the others will be so, too.
+	    MaxSize:    logMaxSize, // megabytes
+	    MaxBackups: logMaxBackups,
+	    MaxAge:     logMaxAge, //days
+	}
+	// Setup the go-logging Logger. (20170812) We have three loggers: one to stderr, one to a logfile, one to syslog for critical stuff. (20170813
+	backendStderr	:= logging.NewLogBackend(os.Stderr, "", 0)
+	backendFile		:= logging.NewLogBackend(rotatingLogger, "", 0)
+	backendSyslog,_	:= logging.NewSyslogBackend("")
+
+	// Set formatting for stderr and file (basically the same). I'm assuming syslog has its own format, but I'll have to see what happens (20170813).
+	backendStderrFormatter	:= logging.NewBackendFormatter(backendStderr, logFormat)
+	backendFileFormatter	:= logging.NewBackendFormatter(backendFile, logFormat)
+
+	// Check if we're overriding the default severity for each backend. This is user-configurable. By default: DEBUG, DEBUG, CRITICAL.
+	// TODO(gwyneth): What about a WebSocket backend using https://github.com/cryptix/exp/wslog ? (20170813)
+	backendStderrLeveled := logging.AddModuleLevel(backendStderrFormatter)
+	backendStderrLeveled.SetLevel(logSeverityStderr, "goslrental")
+	backendFileLeveled := logging.AddModuleLevel(backendFileFormatter)
+	backendFileLeveled.SetLevel(logSeverityFile, "goslrental")
+	backendSyslogLeveled := logging.AddModuleLevel(backendSyslog)
+	backendSyslogLeveled.SetLevel(logSeveritySyslog, "goslrental")
+
+	// Set the backends to be used. Logging should commence now.
+	logging.SetBackend(backendStderrLeveled, backendFileLeveled, backendSyslogLeveled)
+	fmt.Println("Logging set up.")
+}
+
 // main starts here.
 func main() {
+		// to change the flags on the default logger
+	// see https://stackoverflow.com/a/24809859/1035977
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Config viper, which reads in the configuration file every time it's needed.
+	// Note that we need some hard-coded variables for the path and config file name.
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml") // just to make sure; it's the same format as OpenSimulator (or MySQL) config files
+	viper.AddConfigPath("$HOME/go/src/goslrental/") // that's how I have it
+	viper.AddConfigPath("$HOME/go/src/github.com/GwynethLlewelyn/goslrental/") // that's how you'll have it
+	viper.AddConfigPath(".")               // optionally look for config in the working directory
+
+	loadConfiguration() // this gets loaded always, on the first time it runs
+	viper.WatchConfig() // if the config file is changed, this is supposed to reload it (20170811)
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		if (Log == nil) {
+			fmt.Println("Config file changed:", e.Name) // if we couldn't configure the logging subsystem, it's better to print it to the console
+		} else {
+			Log.Info("Config file changed:", e.Name)
+		}
+		loadConfiguration() // I think that this needs to be here, or else, how does Viper know what to call?
+	})
+	
+	// prepares a special channel to look for termination signals
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGCONT)
+
+	// goroutine which listens to signals and calls the loadConfiguration() function if someone sends us a HUP
+	go func() {
+		for {
+	        sig := <-sigs
+	        Log.Notice("Got signal", sig)
+	        switch sig {
+		        case syscall.SIGUSR1:
+		        case syscall.SIGUSR2:
+		        case syscall.SIGHUP:
+		        case syscall.SIGCONT:
+				default:
+		        	Log.Warning("Unknown UNIX signal", sig, "caught!! Ignoring...")
+	        }
+        }
+    }()
+	
 	ql.RegisterDriver()	// this should allow us to use the 'normal' SQL Go bindings to use QL.
-	db, err := sql.Open("ql", "goslrental.db")
+
+	// do some database tests. If it fails, it means the database is broken or corrupted and it's worthless
+	//  to run this application anyway!
+	Log.Info("Testing opening database connection at ", GoBotDSN, "\nPath to static files is:", PathToStaticFiles)
+
+	db, err := sql.Open(PDO_Prefix, GoSLRentalDSN)
 	defer db.Close()
 	if err != nil {
 		log.Fatalf("failed to open db: %s", err)
@@ -67,13 +257,31 @@ func main() {
 
 	err = tx.Commit()	
 	checkErr(err)
+	
+		// Now prepare the web interface
+
+	// Load all templates
+	err = GoSLRentalTemplates.init(PathToStaticFiles + "/templates/*.tpl")
+	checkErr(err) // abort if templates are not found
+
+	http.HandleFunc(URLPathPrefix + "/admin/user-management/",			backofficeUserManagement)
+	
+	http.HandleFunc(URLPathPrefix + "/admin/lsl-register-object/",		backofficeLSLRegisterObject)
+
+	// fallthrough for admin
+	http.HandleFunc(URLPathPrefix + "/admin/",							backofficeMain)
+	http.HandleFunc(URLPathPrefix + "/",								backofficeLogin) // if not auth, then get auth
+
+    err = http.ListenAndServe(ServerPort, nil) // set listen port
+    checkErr(err) // if it can't listen to all the above, then it has to abort anyway
+
 }
 
 // checkErrPanic logs a fatal error and panics.
 func checkErrPanic(err error) {
 	if err != nil {
 		pc, file, line, ok := runtime.Caller(1)
-		log.Panic(filepath.Base(file), ":", line, ":", pc, ok, " - panic:", err)
+		Log.Panic(filepath.Base(file), ":", line, ":", pc, ok, " - panic:", err)
 	}
 }
 
@@ -82,6 +290,6 @@ func checkErrPanic(err error) {
 func checkErr(err error) {
 	if err != nil {
 		pc, file, line, ok := runtime.Caller(1)
-		log.Print(filepath.Base(file), ":", line, ":", pc, ok, " - error:", err)
+		Log.Error(filepath.Base(file), ":", line, ":", pc, ok, " - error:", err)
 	}
 }
